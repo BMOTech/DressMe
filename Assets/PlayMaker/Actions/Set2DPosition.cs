a22d63027c311b10e1927890c5bc38765381cc87
Jim Welch <Jim@welchcompositions.com>
2014-10-04 12:51:48 -0500

    First Commit

---
+++ Assets/PlayMaker/Actions/Set2DPosition.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions {
+	[ActionCategory("Hey Bud")]
+	[Tooltip("Sets the Position of a Game Object. To leave any axis unchanged, set variable to 'None'.")]
+	/**
+     * Set 2D Position
+     * 
+     **/
+	public class Set2DPosition : FsmStateAction {
+		[RequiredField]
+		[Tooltip("The GameObject to position.")]
+		public FsmOwnerDefault gameObject;
+		
+		[UIHint(UIHint.Variable)]
+		[Tooltip("Use a stored Vector2 position, and/or set individual axis below.")]
+		public FsmVector2 vector;
+		
+		public FsmFloat x;
+		public FsmFloat y;
+		public FsmFloat z;
+		
+		[Tooltip("Use local or world space.")]
+		public Space space;
+		
+		[Tooltip("Repeat every frame.")]
+		public bool everyFrame;
+		
+		[Tooltip("Perform in LateUpdate. This is useful if you want to override the position of objects that are animated or otherwise positioned in Update.")]
+		public bool lateUpdate;	
+		
+		public override void Reset() {
+			gameObject = null;
+			vector = null;
+			// default axis to variable dropdown with None selected.
+			x = new FsmFloat { UseVariable = true };
+			y = new FsmFloat { UseVariable = true };
+			z = 0;
+			space = Space.Self;
+			everyFrame = false;
+			lateUpdate = false;
+		}
+		
+		public override void OnEnter() {
+			if (!everyFrame && !lateUpdate) {
+				DoSetPosition();
+				Finish();
+			}		
+		}
+		
+		public override void OnUpdate() {
+			if (!lateUpdate) {
+				DoSetPosition();
+			}
+		}
+		
+		public override void OnLateUpdate() {
+			if (lateUpdate) {
+				DoSetPosition();
+			}
+			
+			if (!everyFrame) {
+				Finish();
+			}
+		}
+		
+		void DoSetPosition() {
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if (go == null) {
+				return;
+			}
+			
+			// init position
+			
+			Vector3 position;
+			
+			if (vector.IsNone) {
+				position = (space == Space.World) ? go.transform.position : go.transform.localPosition;
+			} else {
+				Vector2 ourVector = vector.Value;
+				position.x = ourVector.x;
+				position.y = ourVector.y;
+			}
+			
+			// override any axis
+			
+			if (!x.IsNone) {
+				position.x = x.Value;
+			}
+			if (!y.IsNone) {
+				position.y = y.Value;
+			}
+			position.z = 0;
+			
+			// apply
+			
+			if (space == Space.World) {
+				go.transform.position = position;
+			} else {
+				go.transform.localPosition = position;
+			}
+		}
+		
+		
+	}
+}
---
+++ Assets/Scripts/GrabObject.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrabObject : MonoBehaviour {
+	Transform chicken;
+	Vector3 offSet;    
+	void Update() {
+		Ray ray = camera.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));        // Gets the mouse position in the form of a ray.
+		if (Input.GetMouseButtonDown(0)) {      // If we click the mouse...
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit, Mathf.Infinity)) {        // Then see if an object is beneath us using raycasting.
+				chicken = hit.transform;        // If we hit an object then hold on to the object.
+				offSet = chicken.position-ray.origin;       // This is so when you click on an object its center does not align with mouse position.
+				if (chicken.rigidbody) {
+					chicken.rigidbody.isKinematic = true;
+				}
+			}
+		}
+		else if (Input.GetMouseButtonUp(0)) {
+			if (chicken.rigidbody) {
+				chicken.rigidbody.isKinematic = false;
+			}
+			chicken = null;     // Let go of the object.
+		}
+		if (chicken) {
+			chicken.transform.position = new Vector3(ray.origin.x+offSet.x, chicken.position.y, ray.origin.z+offSet.z);     // Only move the object on a 2D plane.
+		}
+	}
+}
