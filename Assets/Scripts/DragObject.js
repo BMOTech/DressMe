c53b000c39aa16698e855af822ecd9d6687254a4
Brandon Johnson <bjknox865@gmail.com>
2014-10-12 10:38:43 -0500

    old
    old

--- Assets/Scripts/DragObject.js
+++ Assets/Scripts/DragObject.js
@@ -4,11 +4,13 @@ private var object : Transform;     // The object we will move.
 private var offSet : Vector3;       // The object's position relative to the mouse position.
  
 function Update () {
+    var layerMask = 1 << 20;
+    layerMask = ~layerMask;
     var ray = camera.ScreenPointToRay(Input.mousePosition);     // Gets the mouse position in the form of a ray.
     if (Input.GetButtonDown("Fire1")) {     // If we click the mouse...
         if (!object) {      // And we are not currently moving an object...
             var hit : RaycastHit;
-            if (Physics.Raycast(ray, hit, Mathf.Infinity)) {        // Then see if an object is beneath us using raycasting.
+            if (Physics.Raycast(ray, hit, Mathf.Infinity, layerMask)) {        // Then see if an object is beneath us using raycasting.
                 object = hit.transform;     // If we hit an object then hold on to the object.
                 offSet = object.position-ray.origin;        // This is so when you click on an object its center does not align with mouse position.
             }
