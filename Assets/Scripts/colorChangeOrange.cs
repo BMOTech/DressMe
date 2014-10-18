e3df3d90fd733a24666d6a1e1aa896e0259fd058
Jim Welch <Jim@welchcompositions.com>
2014-10-18 16:42:50 -0500

    lots of UI tweaking
    changing over to new Unity UI

---
+++ Assets/Scripts/colorChangeOrange.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour 
+{
+		public Color colorStart = Color.red;
+		public Color colorEnd = Color.green;
+		public float duration = 1.0F;
+		void Update() {
+			float lerp = Mathf.PingPong(Time.time, duration) / duration;
+			renderer.material.color = Color.Lerp(colorStart, colorEnd, lerp);
+		}
+	}
